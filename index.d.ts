e78ccddece31715d5e529eb88dea87a256bba84e
Bo Gosmer <bogosmer@gmail.com>
2018-05-29 10:25:59 +0200

    Very basic typings so it can be used with TypeScript

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+export interface DropzoneComponentProps {
+    djsConfig?: any
+    config?: any
+    eventHandlers?: any
+}
+
+export default class DropzoneComponent extends Component<DropzoneComponentProps, any> {
+    
+}
