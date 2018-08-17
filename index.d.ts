04e9059d8ba5dd45108ca0af8596ff13296af507
Bo Gosmer <bogosmer@gmail.com>
2018-08-17 11:37:44 +0200

    Better generics

--- index.d.ts
+++ index.d.ts
@@ -6,6 +6,6 @@ export interface DropzoneComponentProps {
     eventHandlers?: any
 }
 
-export default class DropzoneComponent<T extends DropzoneComponentProps> extends Component<DropzoneComponentProps, any> {
+export default class DropzoneComponent<T extends DropzoneComponentProps = any> extends Component<T, any> {
     
 }
